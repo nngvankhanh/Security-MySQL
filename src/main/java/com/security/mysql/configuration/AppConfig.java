1362f36a21017a94fa5d0e36b1128dd1b58af436
nguyenvankhanh <vankhanhn735@gmail.com>
2024-05-14 09:25:21 +0700

    change_class_name_Appconfig_to_AppConfig

--- src/main/java/com/security/mysql/configuration/AppConfig.java
+++ src/main/java/com/security/mysql/configuration/AppConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class Appconfig {
+public class AppConfig {
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
